e6c8c34f1b75c37a2c6c24cce89f20865e4d15fc
mahlang03 <62332119+mahlang03@users.noreply.github.com>
2021-10-22 08:31:38 +0100

    Update student.sol

--- contract/student.sol
+++ contract/student.sol
@@ -1,22 +1,11 @@
 pragma solidity >=0.7.0 <0.9.0;
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-interface IERC20Token {
-  function transfer(address, uint256) external returns (bool);
-  function approve(address, uint256) external returns (bool);
-  function transferFrom(address, address, uint256) external returns (bool);
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address) external view returns (uint256);
-  function allowance(address, address) external view returns (uint256);
-
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-}
 
 contract StudentReport {
-    uint internal numStudents;
+    uint internal numStudents = 0;
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
-
+    
     struct Student {
         address admin;
         string name;
@@ -24,48 +13,53 @@ contract StudentReport {
         string department;
         string level;
         string summary;
+        string detail;
     }
-
-    mapping (string => Student) internal students;
-
+    
+    mapping (uint => Student) internal students;
+    
     function addStudent(
         string memory _name,
         string memory _regNum,
         string memory _department,
         string memory _level,
-        string memory _summary
+        string memory _summary,
+        string memory _detail
     ) public {
-
-        students[_regNum] = Student(
+        
+        students[numStudents] = Student(
             msg.sender,
             _name,
             _regNum,
             _department,
             _level,
-            _summary
+            _summary,
+            _detail
         );
         numStudents++;
         }
-
-    function viewStudents(string memory _index) public view returns (
+        
+    function viewStudents(uint _index) public view returns (
         address,
-        string memory,
-        string memory,
-        string memory,
+        string memory, 
+        string memory, 
+        string memory, 
+        string memory, 
         string memory,
         string memory
 
     ) {
         return (
             students[_index].admin,
-            students[_index].name,
-            students[_index].regNum,
-            students[_index].department,
-            students[_index].level,
-            students[_index].summary
+            students[_index].name, 
+            students[_index].regNum, 
+            students[_index].department, 
+            students[_index].level, 
+            students[_index].summary,
+            students[_index].detail
         );
     }
-
+    
     function totalStudents() public view returns (uint) {
         return (numStudents);
     }
